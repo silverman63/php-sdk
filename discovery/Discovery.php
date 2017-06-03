da6773242a3dc26da289f1b0f76ce25a7ee4c438
guojial <guojial@cn.ibm.com>
2017-06-03 17:51:10 +0800

    init Discovery

--- discovery/Discovery.php
+++ discovery/Discovery.php
@@ -13,8 +13,121 @@
  */
  Class Discovery {
  	
- 	
- 	
+ 		/*
+		 * createEnvironment
+		 * @param url
+		 * @param username
+		 * @param password
+		 * 
+		 * @result json
+		 */
+ 		public function createEnvironment($url, $username, $password) {
+
+	        $ch = curl_init(); 
+		    curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+			curl_setopt($ch, CURLOPT_POST, true); 
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($ch, CURLOPT_USERPWD, $username.':'.$password);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			return $result;	
+
+	}
+
+ 		/*
+		 * createEnvironment
+		 * @param url
+		 * @param username
+		 * @param password
+		 * 
+		 * @result json
+		 */
+	public function getEnvironments($url, $username, $password) {
+
+	        $ch = curl_init(); 
+		    curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($ch, CURLOPT_USERPWD, $username.':'.$password);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			return $result;	
+
+	}
+
+ 		/*
+		 * createEnvironment
+		 * @param url
+		 * @param username
+		 * @param password
+		 * @environmentId
+		 * @environmentName
+		 * @environmentDesc
+		 * @size
+		 * 
+		 * @result json
+		 */
+	public function updateEnvironment($url, $username, $password, $environmentId, $environmentName, $environmentDesc, $size) {
+
+			$url = str_replace("{environment_id}", $environmentId, $url);
+
+			$data["name"] = $environmentName;
+			$data["description"] = $environmentDesc;
+			$data["size"] = $size;
+
+			$ch = curl_init(); 
+		    curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data, JSON_UNESCAPED_UNICODE));
+			curl_setopt($ch, CURLOPT_USERPWD, $username.':'.$password);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			return $result;	
+			
+	}
+
+ 		/*
+		 * createEnvironment
+		 * @param url
+		 * @param username
+		 * @param password
+		 * @environmentId
+		 * 
+		 * @result json
+		 */
+	public function deleteEnvironment($url, $username, $password, $environmentId) {
+
+			$url = str_replace("{environment_id}", $environmentId, $url);
+
+			$ch = curl_init(); 
+		    curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($ch, CURLOPT_USERPWD, $username.':'.$password);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			return $result;	
+	}
  	
  }
  
